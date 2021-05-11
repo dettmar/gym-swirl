7433fd746d29f234e93d05c894f1e0706c31ea59
dettmar <dettmar@gmail.com>
2021-05-11 12:01:05 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 # Based on boiler plate https://github.com/openai/gym/blob/master/docs/creating-environments.md
 
-setup(name='gym-swirl',
-	version='0.1',
-	description="Upscale.app's backend upscaling worker",
+setup(name="gym-swirl",
+	version="0.1",
+	description="Simulation of Active Colloids with Social Interaction",
 	author="Johan Dettmar",
-	url="https://github.com/dettmar/swirl",
-	install_requires=['gym', 'torch', 'torchdiffeq']
+	url="https://github.com/dettmar/gym-swirl",
+	license="MIT",
+	install_requires=["gym", "torch", "matplotlib", "jupyter"]
 )
